ea3cb11791d78cb3f6188effc1b3a8e3af842444
nrth3rnlb <206222+nrth3rnlb@users.noreply.github.com>
2025-12-27 13:06:59 +0100

    Revert "Refactor ShelfConstants for clarity and organization"
    This reverts commit a8d6e38f633b9001ed986425b54af34bc6e362d2.

--- shelves/constants.py
+++ shelves/constants.py
@@ -7,41 +7,41 @@ Constants for the Shelves plugin.
 from __future__ import annotations
 
 
+class ShelfConstants:
+    """Central constants for the Shelves plugin."""
 
-"""Central constants for the Shelves plugin."""
+    # noinspection SpellCheckingInspection
+    MUSICBRAINZ_ALBUMID = "musicbrainz_albumid"
+    # noinspection SpellCheckingInspection
+    TAG_KEY = "shelf_name"
 
-# noinspection SpellCheckingInspection
-MUSICBRAINZ_ALBUMID = "musicbrainz_albumid"
-# noinspection SpellCheckingInspection
-TAG_KEY = "shelf_name"
+    # Validation limits
+    # TODO Make configurable
+    MAX_SHELF_NAME_LENGTH = 30
+    MAX_WORD_COUNT = 3
+    INVALID_PATH_CHARS = frozenset(["<", ">", "|", ":", "*", "?", '"', "/", "\\", "'"])
+    ALBUM_INDICATORS = frozenset(["Vol.", "Volume", "Disc", "CD", "Part"])
 
-# Validation limits
-# TODO Make configurable
-MAX_SHELF_NAME_LENGTH = 30
-MAX_WORD_COUNT = 3
-INVALID_PATH_CHARS = frozenset(["<", ">", "|", ":", "*", "?", '"', "/", "\\", "'"])
-ALBUM_INDICATORS = frozenset(["Vol.", "Volume", "Disc", "CD", "Part"])
+    # Config keys
+    CONFIG_MOVE_FILES_TO_KEY = "move_files_to"
+    CONFIG_ACTIVE_TAB = "shelves_active_tab"
+    CONFIG_ALBUM_SHELF_KEY = "shelves_album_shelf"
+    CONFIG_KNOWN_SHELVES_KEY = "shelves_known_shelves"
+    CONFIG_RENAME_SNIPPET_SKELETON_KEY = "shelves_rename_snippet_skeleton"
+    CONFIG_STAGE_1_INCLUDES_NON_SHELVES_KEY = "shelves_stage_1_includes_non_shelves"
+    CONFIG_WORKFLOW_ENABLED_KEY = "shelves_workflow_enabled"
+    CONFIG_WORKFLOW_STAGE_1_SHELVES_KEY = "shelves_workflow_stage_1_shelves"
+    CONFIG_WORKFLOW_STAGE_2_SHELVES_KEY = "shelves_workflow_stage_2_shelves"
 
-# Config keys
-CONFIG_MOVE_FILES_TO_KEY = "move_files_to"
-CONFIG_ACTIVE_TAB = "shelves_active_tab"
-CONFIG_ALBUM_SHELF_KEY = "shelves_album_shelf"
-CONFIG_KNOWN_SHELVES_KEY = "shelves_known_shelves"
-CONFIG_RENAME_SNIPPET_SKELETON_KEY = "shelves_rename_snippet_skeleton"
-CONFIG_STAGE_1_INCLUDES_NON_SHELVES_KEY = "shelves_stage_1_includes_non_shelves"
-CONFIG_WORKFLOW_ENABLED_KEY = "shelves_workflow_enabled"
-CONFIG_WORKFLOW_STAGE_1_SHELVES_KEY = "shelves_workflow_stage_1_shelves"
-CONFIG_WORKFLOW_STAGE_2_SHELVES_KEY = "shelves_workflow_stage_2_shelves"
+    # The value set manually by the user is used
+    SHELF_SOURCE_MANUAL = "manual"
+    MANUAL_SHELF_SUFFIX = f"; {SHELF_SOURCE_MANUAL}"
+    # Value determined by voting is used
+    SHELF_SOURCE_VOTES = "_shelf_votes"
+    # If nothing else works, use the value in TAG_KEY
+    SHELF_SOURCE_FALLBACK = "fallback"
 
-# The value set manually by the user is used
-SHELF_SOURCE_MANUAL = "manual"
-MANUAL_SHELF_SUFFIX = f"; {SHELF_SOURCE_MANUAL}"
-# Value determined by voting is used
-SHELF_SOURCE_VOTES = "_shelf_votes"
-# If nothing else works, use the value in TAG_KEY
-SHELF_SOURCE_FALLBACK = "fallback"
-
-RENAME_SNIPPET = """$set(_shelffolder,$shelf_name())
+    RENAME_SNIPPET = """$set(_shelffolder,$shelf_name())
 $set(_shelffolder,$if($not($eq(%_shelffolder%,)),%_shelffolder%/))
 
 %_shelffolder%
@@ -50,6 +50,6 @@ $if2(%albumartist%,%artist%)/%album%/%title%"""
 
 # Default configuration values
 DEFAULT_SHELVES = {
-ShelfConstants.CONFIG_WORKFLOW_STAGE_1_SHELVES_KEY: "",
-ShelfConstants.CONFIG_WORKFLOW_STAGE_2_SHELVES_KEY: "",
+    ShelfConstants.CONFIG_WORKFLOW_STAGE_1_SHELVES_KEY: "",
+    ShelfConstants.CONFIG_WORKFLOW_STAGE_2_SHELVES_KEY: "",
 }
