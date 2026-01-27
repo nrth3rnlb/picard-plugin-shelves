9a44fafeca2f5c73dc97becc21f4e54de144329e
nrth3rnlb <206222+nrth3rnlb@users.noreply.github.com>
2026-01-27 19:57:17 +0100

    refactor(script_functions): streamline shelf function and remove logging

--- shelves/script_functions.py
+++ shelves/script_functions.py
@@ -4,7 +4,6 @@ Script functions for the Shelves plugin.
 
 from __future__ import annotations
 
-from picard import log
 from picard.script import ScriptParser
 
 from . import utils
@@ -12,13 +11,9 @@ from .workflow import WorkflowEngine
 
 
 def shelf(parser: ScriptParser) -> str:
-    """
-    Picard script function: `$shelf()`
-    """
+    """Picard script function: `$shelf()`"""
     context_shelf, metadata_shelf = utils.squeeze_the_parser(parser)
-
     if not metadata_shelf and not context_shelf:
-        log.error("No shelf found in file metadata or context %s.", parser.file)
-        return WorkflowEngine.apply_transition("")
-    else:
-        return WorkflowEngine.apply_transition(metadata_shelf or context_shelf)
+        return ""
+
+    return WorkflowEngine.apply_transition(metadata_shelf or context_shelf)
